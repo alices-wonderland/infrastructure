34b9f98a3959d5a80dbd7f09001e525582fc06dc
Ukonn Ra <ukonnra@outlook.com>
2021-04-09 17:47:29 +0800

    Reformat code

--- wonderland-project-gradle-plugin/src/main/kotlin/com/ukonnra/wonderland/project/gradle/ProjectPlugin.kt
+++ wonderland-project-gradle-plugin/src/main/kotlin/com/ukonnra/wonderland/project/gradle/ProjectPlugin.kt
@@ -9,6 +9,7 @@ const val TASK_NAME = "wonderlandProject"
 class ProjectPlugin : Plugin<Project> {
   override fun apply(project: Project) {
     val extension = project.extensions.create(EXTENSION_NAME, ProjectPluginExtension::class.java)
+    println("Extensions: $extension")
     project.dependencies.add("implementation", "javax.servlet:javax.servlet-api:4.0.1")
   }
 }
